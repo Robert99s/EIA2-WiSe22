b962dc2cf7768ed9926d8749433983e769dc5288
Robert99s <102170260+Robert99s@users.noreply.github.com>
2022-10-13 18:51:10 +0200

    A01v2

--- A01_RandomPoem/script.js
+++ A01_RandomPoem/script.js
@@ -1,11 +1,28 @@
+/*
+Aufgabe: 1 Zufallsgedicht
+Name: Robert Schindler
+Matrikel 271342
+Datum: 13.10.2022
+Quellen: Gemeinsam mit Henning Pils erarbeitet
+*/
 var RandomPoem;
 (function (RandomPoem) {
-    console.log("Test");
     let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    for (let index = 6; index >= 1; index--) {
-        console.log(subjects, predicates, objects);
+    for (let index = 7; index >= 1; index--) {
+        console.log(getVerse(subjects, predicates, objects));
+    }
+    function getVerse(_subjects, _predicates, _objects) {
+        let verse = "";
+        let subjectsNumber = Math.floor(Math.random() * _subjects.length);
+        let predicatesNumber = Math.floor(Math.random() * _predicates.length);
+        let objectsNumber = Math.floor(Math.random() * _objects.length);
+        verse = _subjects[subjectsNumber] + " " + _predicates[predicatesNumber] + " " + _objects[objectsNumber];
+        _subjects.slice(subjectsNumber, 1);
+        _predicates.slice(predicatesNumber, 1);
+        _objects.slice(objectsNumber, 1);
+        return verse;
     }
 })(RandomPoem || (RandomPoem = {}));
 //# sourceMappingURL=script.js.map
--- A01_RandomPoem/script.ts
+++ A01_RandomPoem/script.ts
@@ -1,10 +1,31 @@
+/*
+Aufgabe: 1 Zufallsgedicht
+Name: Robert Schindler
+Matrikel 271342
+Datum: 13.10.2022
+Quellen: Gemeinsam mit Henning Pils erarbeitet
+*/
+
 namespace RandomPoem {
-    console.log("Test");
     let subjects: String [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let predicates: String [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects: String [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
-    for (let index: number = 6; index >= 1; index--) {
-        console.log(subjects, predicates, objects);
+    for (let index: number = 7; index >= 1; index--) {
+        console.log(getVerse(subjects, predicates, objects));
+    }
+        
+    function getVerse(_subjects: string , _predicates: string, _objects: string): string {
+
+            let verse: string = "";
+            let subjectsNumber: number = Math.floor(Math.random() * _subjects.length);
+            let predicatesNumber: number = Math.floor(Math.random() * _predicates.length);
+            let objectsNumber: number = Math.floor(Math.random() * _objects.length);
+                     
+            verse = _subjects[subjectsNumber] + " " + _predicates[predicatesNumber] + " " + _objects[objectsNumber];
+            _subjects.slice(subjectsNumber, 1);
+            _predicates.slice(predicatesNumber, 1);
+            _objects.slice(objectsNumber, 1);
+            return verse;
         }
 }
