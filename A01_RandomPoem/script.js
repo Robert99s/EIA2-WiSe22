49a571ad04c0ae5539e823ac5f80f341221de849
Robert99s <102170260+Robert99s@users.noreply.github.com>
2022-10-13 12:42:51 +0200

    A01v1

---
+++ A01_RandomPoem/script.js
@@ -0,0 +1,11 @@
+var RandomPoem;
+(function (RandomPoem) {
+    console.log("Test");
+    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    for (let index = 6; index >= 1; index--) {
+        console.log(subjects, predicates, objects);
+    }
+})(RandomPoem || (RandomPoem = {}));
+//# sourceMappingURL=script.js.map
---
+++ A01_RandomPoem/script.ts
@@ -0,0 +1,10 @@
+namespace RandomPoem {
+    console.log("Test");
+    let subjects: String [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let predicates: String [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objects: String [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+    for (let index: number = 6; index >= 1; index--) {
+        console.log(subjects, predicates, objects);
+        }
+}
