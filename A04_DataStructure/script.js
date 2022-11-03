77a2d50b9ea3a80899369785f2ce6ccdccb2ce48
Robert99s <102170260+Robert99s@users.noreply.github.com>
2022-11-03 11:56:57 +0100

    a04v1

---
+++ A04_DataStructure/script.js
@@ -0,0 +1,42 @@
+/*
+A03 ShoppingList
+Robert Schindler MKB
+Matrikel: 271342
+Datum: 29.10.2022
+Quellen: Jonas Atzenhofer und Yannik König
+*/
+var A03_ShoppingList;
+(function (A03_ShoppingList) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        document.querySelector("#item").addEventListener("keydown", itemName);
+        document.querySelector("#amount").addEventListener("keydown", amountNumber);
+        document.querySelector("#comment").addEventListener("keydown", commentInfos);
+        document.querySelector("#addButton").addEventListener("click", addItem);
+        document.querySelector("#bought").addEventListener("click", itemBought);
+        document.querySelector("#editButton").addEventListener("click", editItem);
+        document.querySelector("#deleteButton").addEventListener("click", deleteItem);
+    }
+    function itemName() {
+        console.log("Item eintragen");
+    }
+    function amountNumber() {
+        console.log("Menge eintragen");
+    }
+    function commentInfos() {
+        console.log("Maßeinheit und Zusatzinfos eintragen");
+    }
+    function addItem() {
+        console.log("Item zur Liste hinzügen");
+    }
+    function itemBought() {
+        console.log("Item gekauft");
+    }
+    function editItem() {
+        console.log("Item editieren");
+    }
+    function deleteItem() {
+        console.log("Item löschen");
+    }
+})(A03_ShoppingList || (A03_ShoppingList = {}));
+//# sourceMappingURL=script.js.map
---
+++ A04_DataStructure/script.ts
@@ -0,0 +1,50 @@
+/*
+A03 ShoppingList
+Robert Schindler MKB
+Matrikel: 271342
+Datum: 29.10.2022
+Quellen: Jonas Atzenhofer und Yannik König
+*/
+
+namespace A03_ShoppingList {
+    window.addEventListener("load", handleLoad);
+    
+    function handleLoad(): void {
+        document.querySelector("#item").addEventListener("keydown", itemName);
+        document.querySelector("#amount").addEventListener("keydown", amountNumber);
+        document.querySelector("#comment").addEventListener("keydown", commentInfos);
+        document.querySelector("#addButton").addEventListener("click", addItem);
+        document.querySelector("#bought").addEventListener("click", itemBought);
+        document.querySelector("#editButton").addEventListener("click", editItem);
+        document.querySelector("#deleteButton").addEventListener("click", deleteItem);
+    }
+
+    function itemName(): void {
+        console.log("Item eintragen");
+    }
+
+    function amountNumber(): void {
+        console.log("Menge eintragen");
+    }
+
+    function commentInfos(): void {
+        console.log("Maßeinheit und Zusatzinfos eintragen");
+    }
+
+    function addItem(): void {
+        console.log("Item zur Liste hinzügen"); 
+    }
+
+    function itemBought(): void {
+        console.log("Item gekauft");   
+    }
+
+    function editItem(): void {
+        console.log("Item editieren");
+    }
+
+    function deleteItem(): void {
+        console.log("Item löschen");
+        
+    }
+}
