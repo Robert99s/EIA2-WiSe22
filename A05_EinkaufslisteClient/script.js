302b0b11db6246be7651af1226d156eace9a42d2
Robert99s <102170260+Robert99s@users.noreply.github.com>
2022-11-10 20:47:06 +0100

    a05v5

--- A05_EinkaufslisteClient/script.js
+++ A05_EinkaufslisteClient/script.js
@@ -2,70 +2,112 @@
 A05 EinkaufslisteClient
 Robert Schindler MKB
 Matrikel: 271342
-Datum: 05.11.2022
-Quellen: Jonas Atzenhofer, Yannik König und Henning Pils
+Datum: 10.11.2022
+Quellen: Yannik König!, Jonas Atzenhofer, Henning Pils und Tristan Broghammer
 */
 var A05_EinkaufslisteClient;
 (function (A05_EinkaufslisteClient) {
+    let date = new Date();
+    let dateWithoutTime = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
     let itemNumber = 0;
     window.addEventListener("load", handleLoad);
-    function handleLoad() {
+    async function handleLoad(_event) {
         let addButton = document.querySelector("button#add");
         addButton.addEventListener("click", itemAdd);
+        let response = await fetch("https://robert99s.github.io/EIA2-WiSe22/A05_EinkaufslisteClient/data.json");
+        let item = await response.text();
+        let data = JSON.parse(item);
+        generateExistingItem(data);
     }
-    function itemAdd() {
+    function generateExistingItem(_data) {
+        let values = _data[1];
+        console.log(values[0].newItem);
+        let newItem = values[0].newItem;
+        let amount = values[0].amount;
+        let comment = values[0].comment;
+        let list = document.getElementById("list");
+        let newDiv = document.createElement("div");
+        let newInput = document.createElement("input");
+        let divItemData = document.createElement("div");
+        createInput(newInput, newDiv);
+        createDiv(newDiv);
+        createItemDiv(divItemData, newDiv);
+        addElement(divItemData);
+        addElement(divItemData, newItem.toString());
+        addElement(divItemData, amount.toString());
+        addElement(divItemData, comment.toString());
+        addElement(divItemData, dateWithoutTime);
+        addButton(newDiv, "edit");
+        addButton(newDiv, "delete");
+        list.appendChild(newDiv);
+    }
+    async function itemAdd() {
         let formData = new FormData(document.querySelector("form"));
         let newItem = formData.get("newItem");
         let amount = formData.get("amount");
         let comment = formData.get("comment");
-        let bought = false;
-        let date = "05.11.2022";
-        itemNumber++;
-        let list = document.getElementById("shoppingList");
+        let list = document.getElementById("list");
         let newDiv = document.createElement("div");
-        newDiv.setAttribute("class", "lister");
-        newDiv.setAttribute("id", "lister" + itemNumber);
         let newInput = document.createElement("input");
-        newDiv.appendChild(newInput);
-        newInput.setAttribute("class", "bought");
-        newInput.setAttribute("id", "bought" + itemNumber);
-        newInput.setAttribute("type", "checkbox");
-        newInput.addEventListener("change", itemBought);
         let divItemData = document.createElement("div");
-        newDiv.appendChild(divItemData);
-        divItemData.setAttribute("class", "ItemData");
-        divItemData.setAttribute("id", "ItemData" + itemNumber);
-        let newItemField = document.createElement("p");
-        divItemData.appendChild(newItemField);
-        newItemField.setAttribute("class", "ItemDataFont");
-        newItemField.innerHTML = newItem;
-        let amountField = document.createElement("p");
-        divItemData.appendChild(amountField);
-        amountField.setAttribute("class", "ItemDataFont");
-        amountField.innerHTML = amount;
-        let commentField = document.createElement("p");
-        divItemData.appendChild(commentField);
-        commentField.setAttribute("class", "ItemDataFont");
-        commentField.innerHTML = comment;
-        let dateField = document.createElement("p");
-        divItemData.appendChild(dateField);
-        dateField.setAttribute("class", "ItemDataFont");
-        dateField.innerHTML = date;
-        let editButton = document.createElement("button");
-        newDiv.appendChild(editButton);
-        editButton.setAttribute("class", "edit");
-        editButton.setAttribute("id", "edit" + itemNumber);
-        editButton.setAttribute("type", "button");
-        editButton.innerHTML = "edit";
-        editButton.addEventListener("click", editItem);
-        let deleteButton = document.createElement("button");
-        newDiv.appendChild(deleteButton);
-        deleteButton.setAttribute("class", "delete");
-        deleteButton.setAttribute("id", "delete" + itemNumber);
-        deleteButton.setAttribute("type", "button");
-        deleteButton.innerHTML = "delete";
-        deleteButton.addEventListener("click", deleteItem);
+        let bought = false;
+        itemNumber++;
+        createInput(newInput, newDiv);
+        createDiv(newDiv);
+        createItemDiv(divItemData, newDiv);
+        addElement(divItemData);
+        addElement(divItemData, newItem.toString());
+        addElement(divItemData, amount.toString());
+        addElement(divItemData, comment.toString());
+        addElement(divItemData, dateWithoutTime);
+        addButton(newDiv, "edit");
+        addButton(newDiv, "delete");
         list.appendChild(newDiv);
+        let query = new URLSearchParams(formData);
+        await fetch("index.html" + "?" + query.toString());
+        console.log(query.toString());
+        alert("Item is added to the list");
+    }
+    function addElement(_parent, _content) {
+        let newItemField = document.createElement("p");
+        _parent.appendChild(newItemField);
+        newItemField.setAttribute("class", "ItemDataFont");
+        if (_content) {
+            newItemField.innerHTML = _content;
+        }
+    }
+    function addButton(_parent, _identify) {
+        let newButton = document.createElement("button");
+        _parent.appendChild(newButton);
+        newButton.setAttribute("class", _identify);
+        newButton.setAttribute("id", _identify + itemNumber);
+        newButton.setAttribute("type", "button");
+        newButton.innerHTML = _identify;
+        switch (_identify) {
+            case "edit":
+                newButton.addEventListener("click", editItem);
+                break;
+            case "delete":
+                newButton.addEventListener("click", deleteItem);
+            default:
+                break;
+        }
+    }
+    function createDiv(_element) {
+        _element.setAttribute("class", "lister");
+        _element.setAttribute("id", "lister" + itemNumber);
+    }
+    function createInput(_element, _parent) {
+        _parent.appendChild(_element);
+        _element.setAttribute("class", "bought");
+        _element.setAttribute("id", "bought" + itemNumber);
+        _element.setAttribute("type", "checkbox");
+        _element.addEventListener("change", itemBought);
+    }
+    function createItemDiv(_element, _parent) {
+        _parent.appendChild(_element);
+        _element.setAttribute("class", "ItemData");
+        _element.setAttribute("id", "ItemData" + itemNumber);
     }
     function itemBought(_event) {
         let trigger = _event.target.id;
@@ -82,7 +124,7 @@ var A05_EinkaufslisteClient;
         let trigger = _event.target.id;
         let triggerNum = trigger.replace(/\D/g, "");
         let identifier = parseInt(triggerNum);
-        let list = document.getElementById("shoppingList");
+        let list = document.getElementById("list");
         let remIt = document.getElementById("lister" + identifier);
         list.removeChild(remIt);
     }
--- A05_EinkaufslisteClient/script.ts
+++ A05_EinkaufslisteClient/script.ts
@@ -2,113 +2,176 @@
 A05 EinkaufslisteClient
 Robert Schindler MKB
 Matrikel: 271342
-Datum: 05.11.2022
-Quellen: Jonas Atzenhofer, Yannik König und Henning Pils
+Datum: 10.11.2022
+Quellen: Yannik König!, Jonas Atzenhofer, Henning Pils und Tristan Broghammer
 */
 
 namespace A05_EinkaufslisteClient {
-    let itemNumber: number = 0; 
+
+    let date: Date = new Date();
+    let dateWithoutTime: string = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+
+    let itemNumber: number = 0;
 
     window.addEventListener("load", handleLoad);
 
-    function handleLoad(): void {
-        
-        let addButton: HTMLButtonElement = document.querySelector("button#add");
-        addButton.addEventListener("click", itemAdd); 
+    interface ItemAdded {
+        bought: boolean;
+        newItem: string;
+        amount: number;
+        comment: string;
+        date: string;
 
     }
-    function itemAdd(): void {
-        let formData: FormData = new FormData(document.querySelector("form"));
-        let newItem: FormDataEntryValue = formData.get("newItem"); 
-        let amount: FormDataEntryValue = formData.get("amount"); 
-        let comment: FormDataEntryValue = formData.get("comment"); 
-        let bought: boolean = false; 
-        let date: string = "05.11.2022"; 
-        itemNumber++;
 
-        let list: HTMLElement = document.getElementById("shoppingList");
+    interface Data {
+        [1]: ItemAdded[];
+    }
 
+    async function handleLoad(_event: Event): Promise<void> {
+        let addButton: HTMLButtonElement = document.querySelector("button#add");
+        addButton.addEventListener("click", itemAdd);
+
+        let response: Response = await fetch("https://robert99s.github.io/EIA2-WiSe22/A05_EinkaufslisteClient/data.json");
+        let item: string = await response.text();
+        let data: Data = JSON.parse(item);
+
+        generateExistingItem(data);
+    }
+
+    function generateExistingItem(_data: Data): void {
+        let values: ItemAdded[] = _data[1];
+        console.log(values[0].newItem);
+
+        let newItem: string = values[0].newItem;
+        let amount: number = values[0].amount;
+        let comment: string = values[0].comment;
+        let list: HTMLElement = document.getElementById("list");
         let newDiv: HTMLDivElement = document.createElement("div");
-        newDiv.setAttribute("class", "lister"); 
-        newDiv.setAttribute("id", "lister" + itemNumber); 
-
-        let newInput: HTMLInputElement = document.createElement("input"); 
-        newDiv.appendChild(newInput); 
-        newInput.setAttribute("class", "bought"); 
-        newInput.setAttribute("id", "bought" + itemNumber); 
-        newInput.setAttribute("type", "checkbox");
-        newInput.addEventListener("change", itemBought); 
-
+        let newInput: HTMLInputElement = document.createElement("input");
         let divItemData: HTMLDivElement = document.createElement("div");
-        newDiv.appendChild(divItemData); 
-        divItemData.setAttribute("class", "ItemData");
-        divItemData.setAttribute("id", "ItemData" + itemNumber); 
 
-        /*
+        createInput(newInput, newDiv); 
+
+        createDiv(newDiv); 
+
+        createItemDiv(divItemData, newDiv); 
+
+        addElement(divItemData);
+
+        addElement(divItemData, newItem.toString());
+
+        addElement(divItemData, amount.toString());
+
+        addElement(divItemData, comment.toString()); 
+
+        addElement(divItemData, dateWithoutTime); 
+
+        addButton(newDiv, "edit"); 
+
+        addButton(newDiv, "delete"); 
+
+        list.appendChild(newDiv);
+    }
+        
+    async function itemAdd(): Promise<void> {
+        let formData: FormData = new FormData(document.querySelector("form"));
+        let newItem: FormDataEntryValue = formData.get("newItem");
+        let amount: FormDataEntryValue = formData.get("amount");
+        let comment: FormDataEntryValue = formData.get("comment");
+        let list: HTMLElement = document.getElementById("list");
+        let newDiv: HTMLDivElement = document.createElement("div");
+        let newInput: HTMLInputElement = document.createElement("input");
+        let divItemData: HTMLDivElement = document.createElement("div");
+        let bought: boolean = false;
+        itemNumber++;
+ 
+
+        createInput(newInput, newDiv);
+
+        createDiv(newDiv);
+
+        createItemDiv(divItemData, newDiv);
+
+        addElement(divItemData);
+
+        addElement(divItemData, newItem.toString());
+
+        addElement(divItemData, amount.toString());
+
+        addElement(divItemData, comment.toString());
+
+        addElement(divItemData, dateWithoutTime);
+
+        addButton(newDiv, "edit");
+
+        addButton(newDiv, "delete");
+
+        list.appendChild(newDiv);
+
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        await fetch("index.html" + "?" + query.toString());
+        console.log(query.toString());
+        alert ("Item is added to the list");
+    }
+
+    function addElement(_parent: HTMLElement, _content?: string): void {
         let newItemField: HTMLElement = document.createElement("p");
-        divItemData.appendChild(newItemField);
+        _parent.appendChild(newItemField);
         newItemField.setAttribute("class", "ItemDataFont");
-        newItemField.innerHTML = (newItem as string); 
-
-        let amountField: HTMLElement = document.createElement("p");
-        divItemData.appendChild(amountField);
-        amountField.setAttribute("class", "ItemDataFont");
-        amountField.innerHTML = (amount as string); 
-
-        let commentField: HTMLElement = document.createElement("p");
-        divItemData.appendChild(commentField);
-        commentField.setAttribute("class", "ItemDataFont");
-        commentField.innerHTML = (comment as string); 
-
-        let dateField: HTMLElement = document.createElement("p");
-        divItemData.appendChild(dateField);
-        dateField.setAttribute("class", "ItemDataFont");
-        dateField.innerHTML = (date as string);
-        */
-
-        /*
-        function addElement(_parent: HTMLElement, _content?: string): void {
-            let newButton: HTMLButtonElement = document.createElement("button");
-            _parent.appendChild(newItemField);
-            newItemField.setAttribute("class", "ItemDataFont");
-            if (_content) {
-                newItemField.innerHTML = (_content as string);
-            }
+        if (_content) {
+            newItemField.innerHTML = (_content as string);
         }
-        */
+    }
 
-        let editButton: HTMLButtonElement = document.createElement("button");  
-        newDiv.appendChild(editButton); 
-        editButton.setAttribute("class", "edit");
-        editButton.setAttribute("id", "edit" + itemNumber);
-        editButton.setAttribute("type", "button");
-        editButton.innerHTML = "edit"; 
-        editButton.addEventListener("click", editItem);
+    function addButton(_parent: HTMLElement, _identify: string): void {
+        let newButton: HTMLElement = document.createElement("button");
+        _parent.appendChild(newButton);
+        newButton.setAttribute("class", _identify);
+        newButton.setAttribute("id", _identify + itemNumber);
+        newButton.setAttribute("type", "button");
+        newButton.innerHTML = _identify;
+        switch (_identify) {
+            case "edit":
+                newButton.addEventListener("click", editItem);
+                break;
+            case "delete":
+                newButton.addEventListener("click", deleteItem);
+            default:
+                break;
+        }
+    }
 
-        let deleteButton: HTMLButtonElement = document.createElement("button");
-        newDiv.appendChild(deleteButton); 
-        deleteButton.setAttribute("class", "delete");
-        deleteButton.setAttribute("id", "delete" + itemNumber);
-        deleteButton.setAttribute("type", "button"); 
-        deleteButton.innerHTML = "delete"; 
-        deleteButton.addEventListener("click", deleteItem); 
+    function createDiv(_element: HTMLElement): void {
+        _element.setAttribute("class", "lister");
+        _element.setAttribute("id", "lister" + itemNumber);
+    }
 
-        list.appendChild(newDiv); 
+    function createInput(_element: HTMLElement, _parent: HTMLElement): void {
+        _parent.appendChild(_element);
+        _element.setAttribute("class", "bought");
+        _element.setAttribute("id", "bought" + itemNumber);
+        _element.setAttribute("type", "checkbox");
+        _element.addEventListener("change", itemBought);
+    }
+
+    function createItemDiv(_element: HTMLElement, _parent: HTMLElement): void {
+        _parent.appendChild(_element);
+        _element.setAttribute("class", "ItemData");
+        _element.setAttribute("id", "ItemData" + itemNumber);
     }
 
     function itemBought(_event: Event): void {
         let trigger: string = (_event.target as HTMLInputElement).id;
         let triggerNum: string =  trigger.replace(/\D/g, "");
         let identifier: number = parseInt(triggerNum);
-        console.log("gekauft");
-        
+        console.log("gekauft");    
     }
 
     function editItem(_event: Event): void {
         let trigger: string = (_event.target as HTMLButtonElement).id;
         let triggerNumber: string =  trigger.replace(/\D/g, "");
         console.log("editieren");
-        
     }
 
     function deleteItem(_event: Event): void {
@@ -116,9 +179,8 @@ namespace A05_EinkaufslisteClient {
         let triggerNum: string = trigger.replace(/\D/g, "");
         let identifier: number = parseInt(triggerNum); 
 
-        let list: HTMLElement = document.getElementById("shoppingList");
+        let list: HTMLElement = document.getElementById("list");
         let remIt: HTMLElement = document.getElementById("lister" + identifier);
         list.removeChild(remIt); 
     }
-
 }
